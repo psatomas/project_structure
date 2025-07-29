04eb3fb2a0540087ae12736876f9b29b0a4d9333
psatomas <psatomas@gmail.com>
2025-07-29 20:49:11 -0300

    refactor: re-export inventory items for cleaner imports

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,8 +1,13 @@
 pub mod products;
 
+pub use products::{Item, ProductCategory};
+
 pub const FLOOR_SPACE: i32 = 10000;        
 pub const MANAGER: &str = "Ivan Invetory";
 fn talk_to_manager() {
-    println!("Hey, {}, how's your coffee?", MANAGER);
+    println!("Hey, {}, how's your coffee? What do you think of {:?}",
+        MANAGER,
+        ProductCategory::Ladder
+    );
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 mod inventory;
 mod orders;
 
-use inventory::products::{Item, ProductCategory};
-use inventory::{FLOOR_SPACE, MANAGER as INVENTORY_MANAGER};
+use inventory::{Item, ProductCategory, FLOOR_SPACE, MANAGER as INVENTORY_MANAGER};
 use orders::MANAGER as ORDERS_MANAGER;
 
 fn main() {
     println!(
         "Our managers are {} and {}. We have {} square feet of floor space",
-        INVENTORY_MANAGER,
-        ORDERS_MANAGER,
-        FLOOR_SPACE
+        INVENTORY_MANAGER, ORDERS_MANAGER, FLOOR_SPACE
     );
 
     let favorite_category = ProductCategory::Hammer;
