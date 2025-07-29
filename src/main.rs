50fd83fda1c7d7bb78c3022496c5a6b8aa07695c
psatomas <psatomas@gmail.com>
2025-07-29 06:13:30 -0300

    feat: integrate inventory functionality into main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ mod inventory;
 mod orders;
 
 fn main() {
-    println!("The manager of our invertory is: {}", inventory::MANAGER);
-    println!("The manager of our orders is: {}", orders::MANAGER);
+    println!(
+        "Our managers are {} and {}. We have {} square feet of floor space",
+        inventory::MANAGER,
+        orders::MANAGER,
+        inventory::FLOOR_SPACE
+    );
+
+    inventory::talk_to_manager();
+
+    let favorite_category = inventory::ProductCategory::Hammer;
+    println!("My favorite category of item is {favorite_category:?}");
+
+    let tall_ladder = inventory::Item {
+        name: String::from("Ladder-o-matic 2000"),
+        category: favorite_category,
+        quantity: 100,
+    };
+    println!("{:#?}", tall_ladder);
 }
